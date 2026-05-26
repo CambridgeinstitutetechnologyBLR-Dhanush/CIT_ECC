f18007de9899bf90553dda6c08cccff38579114e
Dhanush Kulkarni <dhanush.24ece@cambridge.edu.in>
2026-05-26 11:53:04 +0000

    RTL to GDS flow working for ECC Scalar Multiplication

--- test/tb.v
+++ test/tb.v
@@ -1,53 +1,53 @@
-`default_nettype none
 `timescale 1ns/1ps
 
-module tb ();
+module tb;
 
-    reg clk;
-    reg rst_n;
+reg [7:0] ui_in;
+wire [7:0] uo_out;
 
-    reg [7:0] ui_in;
-    reg [7:0] uio_in;
+reg [7:0] uio_in;
+wire [7:0] uio_out;
+wire [7:0] uio_oe;
 
-    wire [7:0] uo_out;
-    wire [7:0] uio_out;
-    wire [7:0] uio_oe;
+reg ena;
+reg clk;
+reg rst_n;
 
-    initial begin
-        $dumpfile("tb.vcd");
-        $dumpvars(0, tb);
-    end
+tt_um_ecc_scalar dut (
+    .ui_in(ui_in),
+    .uo_out(uo_out),
+    .uio_in(uio_in),
+    .uio_out(uio_out),
+    .uio_oe(uio_oe),
+    .ena(ena),
+    .clk(clk),
+    .rst_n(rst_n)
+);
 
-    tt_um_ecc_scalar dut (
-        .ui_in(ui_in),
-        .uo_out(uo_out),
+always #5 clk = ~clk;
 
-        .uio_in(uio_in),
-        .uio_out(uio_out),
-        .uio_oe(uio_oe),
+initial begin
 
-        .ena(1'b1),
-        .clk(clk),
-        .rst_n(rst_n)
-    );
+    $dumpfile("tb.vcd");
+    $dumpvars(0, tb);
 
-    always #5 clk = ~clk;
+    clk = 0;
+    rst_n = 0;
+    ena = 1;
 
-    initial begin
+    ui_in = 8'h00;
+    uio_in = 8'h00;
 
-        clk = 0;
-        rst_n = 0;
+    #20;
 
-        ui_in = 8'b01010011;
-        uio_in = 0;
+    rst_n = 1;
 
-        #10;
+    ui_in = 8'h05;
 
-        rst_n = 1;
+    #100;
 
-        #50;
+    $finish;
 
-        $finish;
-    end
+end
 
 endmodule
